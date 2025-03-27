d537881e6944b6e8d7f0219feee88b76559771f5
tineojf <92743762+tineodev@users.noreply.github.com>
2025-03-27 11:15:25 -0500

    feat: add usermodel class

--- src/java/model/UserModel.java
+++ src/java/model/UserModel.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author tineo
- */
 public class UserModel {
-    
+
+    private int userID;
+    private String username;
+    private String password;
+
+    public UserModel(int userID, String username, String password) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" + "userID=" + userID + ", username=" + username + ", password=" + password + '}';
+    }
 }
